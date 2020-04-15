42efb14b9c4623004ab6d086e8b8b29eb998d172
rahmatulfajriah <60224696+rahmatulfajriah@users.noreply.github.com>
2020-04-15 13:12:23 +0700

    Update Utama.c

--- Utama.c
+++ Utama.c
@@ -9,11 +9,15 @@
 #include "olah_matriks.h"
 #include "input.h"
 #include "animate1.h"
-#include "ouput.h"
+#include "output.h"
+
+
 
 int main()
+
 {
 	// Tampilan awal dekripsi permainan
+
 	printf("***---------------***Selamat datang dalam permainan Game of Life***---------------*** \n");
 	printf("Aturan permainan \n");
 	printf("1. Masukan seed awal permainan \n");
@@ -23,67 +27,102 @@ int main()
 	printf("5. Menu Quit untuk keluar dari permainan \n");
 	
 	// meminta inputan seed dan memasukan ke matriks awal asumsikan seed awal yang dimasukan benar
-	int keluar = 0;
-	while (keluar == 0)
-	{
-		char seed[100];
-		int a,b;
-		printf ("Masukan seed awal : ");
-		gets(seed);
-		a = getRow(seed);
-		b = getColumn(seed);
-		iterasi[i][j] = getSeed(a, b, seed);
+	char seed[100];
+	printf ("Masukan seed awal : ");
+	gets(seed);
+	a = getRow(char seed[100]);
+	b = getColumn(char seed[100]);
+	iterasi[i][j] = getSeed(int a, int b, char seed[100]);*/
 	
-		// pilihan menu
-		int pilih, n,m,a,b;
-		printf("Menu : \n");
-		printf("1.Tick\n");
-		printf("1.Animate\n");
-		printf("3.Quit\n");
-		printf("Masukan pilhan : ");
-		scanf ("%d", &pilih);
-		
+	// pilihan menu
+	int pilih, n,m;
+	printf("Menu : \n");
+	printf("1.Tick\n");
+	printf("1.Animate\n");
+	printf("3.Quit\n");
+	printf("Masukan pilhan : ");
+	scanf ("%d", &pilih);
+	
+		while (pilih != 3)
+
+	{
+
 		while (pilih !=1 && pilih != 2)
+
 		{
+
 			printf("Pilihan menu anda salah\n");
+
 			printf("Masukan pilhan : ");
+
 			scanf ("%d", &pilih);
+
 		}
-		switch(pilih){
-		case'1' :
-			pick(char iterasi[i][j], int a, int b);
-			cetak(char iterasi[i][j], int a, int b);
-		break;
-		case'2' :
+		if (pilih == 1)
+		{
+
+			/*pick(char iterasi[i][j], int a, int b);
+
+			cetak(char iterasi[i][j], int a, int b);*/
+		}
+		if (pilih == 2)
+		{
 			printf ("jumlah iterasi : ");
+
 			scanf("%d", &n);
+
 			for(m=0; m<n ; m++)
+
 			{
-				pick(char iterasi[i][j], int a, int b);
-				cetak(char iterasi[i][j], int a, int b);
+				printf("sayang kamu\n");
+				/*pick(char iterasi[i][j], int a, int b);
+
+				cetak(char iterasi[i][j], int a, int b);*/
+
 			}
-		break;
-		case'3' :
-				char kel;
-				printf ("lanjutkan permainan Y/N : ")
-				scanf("%c", &kel);
-				if (kel ='N');
-				{
-					keluar = 1;
-				}
-		break;
 		}
+
 		printf("Menu : \n");
+
 		printf("1.Tick\n");
+
 		printf("1.Animate\n");
+
 		printf("3.Quit\n");
+
 		printf("Masukan pilhan : ");
+
 		scanf ("%d", &pilih);
+
 	}	
+	while (pilih == 3)
+	{
+		printf ("Masukan seed awal : \n");
+		gets(seed);
+
+	/*a = getRow(char seed[100]);
+
+	b = getColumn(char seed[100]);
+
+	iterasi[i][j] = getSeed(int a, int b, char seed[100]);*/
+
+	// pilihan menu
+		printf("Menu : \n");
+
+		printf("1.Tick\n");
+
+		printf("1.Animate\n");
+
+		printf("3.Quit\n");
+
+		printf("Masukan pilhan : ");
+
+		scanf ("%d", &pilih);
+	}
 	return 0;
 }
-		 
-	
+
+
 
 	
 	
